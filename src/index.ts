383a6f29feadd7888877ff51d5da0bd9902dbd07
boatnoodle <nattasit31@gmail.com>
2021-10-15 15:55:05 +0700

    Fix bot

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,12 @@ import { Probot } from "probot";
 export = (app: Probot) => {
   app.on(["pull_request.opened"], async (context) => {
     const issueComment = context.issue({
-      body: "thanks for open pr",
+      body: "This pull request is in progress.",
     });
     await context.octokit.issues.createComment(issueComment);
 
     const issueLabel = context.issue({
-      labels: ["bug"],
+      labels: [":construction:  wip"],
     });
     await context.octokit.issues.addLabels(issueLabel);
   });
