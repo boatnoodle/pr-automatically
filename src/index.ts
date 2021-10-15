1617d6b877c78734d415b062f2348f8013abb3df
boatnoodle <nattasit31@gmail.com>
2021-10-15 15:19:39 +0700

    Fix bot

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,9 @@
 import { Probot } from "probot";
 
 export = (app: Probot) => {
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    await context.octokit.issues.createComment(issueComment);
-  });
-
   app.on("pull_request.opened", async (context) => {
     const issueLabel = context.issue({
-      labels: [":construction:  wip"],
+      labels: ["bug"],
     });
     await context.octokit.issues.addLabels(issueLabel);
   });
