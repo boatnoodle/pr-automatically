f359daf27a7143dc41cceabbd8fae23a7695fd38
boatnoodle <nattasit31@gmail.com>
2021-10-15 15:31:19 +0700

    Fix bot

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import { Probot } from "probot";
 
 export = (app: Probot) => {
-  app.on(["issues.opened", "pull_request.opened"], async (context) => {
+  app.on(["pull_request.opened"], async (context) => {
+    const payload = context.pullRequest({
+      body: "thanks for open pull request",
+    });
+    await context.octokit.pulls.createReviewComment(payload);
+  });
+
+  app.on(["issues.opened"], async (context) => {
     const issueLabel = context.issue({
       body: "thanks for react",
     });
